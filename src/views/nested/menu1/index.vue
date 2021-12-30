b3e801c2bfae6bcc02e120b7ffbef7d61aab5d8b
“张智皓” <zhang948498@163.com>
2021-12-30 15:25:36 +0800

    fix: 🐛 router-view

--- src/views/nested/menu1/index.vue
+++ src/views/nested/menu1/index.vue
@@ -1,7 +1,9 @@
 <template>
-    <div style="padding:30px;">
-        <el-alert :closable="false" title="menu 1">
-            <router-view />
-        </el-alert>
-    </div>
+    <router-view v-slot="{ Component }">
+        <div style="padding: 30px">
+            <el-alert :closable="false" title="menu 1">
+                <component :is="Component" />
+            </el-alert>
+        </div>
+    </router-view>
 </template>
--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -1,7 +1,9 @@
 <template>
-    <div style="padding:30px;">
-        <el-alert :closable="false" title="menu 1-1" type="success">
-            <router-view />
-        </el-alert>
-    </div>
+    <router-view v-slot="{ Component }">
+        <div style="padding: 30px">
+            <el-alert :closable="false" title="menu 1-1" type="success">
+                <component :is="Component" />
+            </el-alert>
+        </div>
+    </router-view>
 </template>
