148c2d4b36792569f69cbf685dbbc421ecb40803
“张智皓” <zhang948498@163.com>
2022-01-20 17:55:45 +0800

    feat: 🎸 use jsx

--- src/layout/components/Sidebar/FixiOSBug.js
+++ src/layout/components/Sidebar/FixiOSBug.js
@@ -1,26 +1,28 @@
-export default {
-    computed: {
-        device() {
-            return this.$store.state.app.device;
+import { computed, onMounted, ref } from 'vue';
+import { useStore } from 'vuex';
+
+export const useFixiOSBug = () => {
+    const store = useStore();
+    const device = computed(() => store.state.app.device);
+    const subMenu = ref(null);
+    onMounted(() => {
+        // In order to fix the click on menu on the ios device will trigger the mouseleave bug
+        // https://github.com/PanJiaChen/vue-element-admin/issues/1135
+        fixBugIniOS();
+    });
+    const fixBugIniOS = () => {
+        const $subMenu = subMenu;
+        if ($subMenu) {
+            const handleMouseleave = $subMenu.handleMouseleave;
+            $subMenu.handleMouseleave = e => {
+                if (device.value === 'mobile') {
+                    return;
+                }
+                handleMouseleave(e);
+            };
         }
-    },
-    mounted() {
-    // In order to fix the click on menu on the ios device will trigger the mouseleave bug
-    // https://github.com/PanJiaChen/vue-element-admin/issues/1135
-        this.fixBugIniOS();
-    },
-    methods: {
-        fixBugIniOS() {
-            const $subMenu = this.$refs.subMenu;
-            if ($subMenu) {
-                const handleMouseleave = $subMenu.handleMouseleave;
-                $subMenu.handleMouseleave = (e) => {
-                    if (this.device === 'mobile') {
-                        return;
-                    }
-                    handleMouseleave(e);
-                };
-            }
-        }
-    }
+    };
+    return {
+        subMenu,
+    };
 };
--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,3 +1,61 @@
+<script setup>
+import path from 'path-browserify';
+import { isExternal } from '@/utils/validate';
+import Item from './Item.jsx';
+import AppLink from './Link.vue';
+import { useFixiOSBug } from './FixiOSBug';
+import { ref } from 'vue';
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true,
+    },
+    isNest: {
+        type: Boolean,
+        default: false,
+    },
+    basePath: {
+        type: String,
+        default: '',
+    },
+});
+const { subMenu } = useFixiOSBug();
+const onlyOneChild = ref(null);
+const hasOneShowingChild = (children = [], parent) => {
+    const showingChildren = children.filter(item => {
+        if (item.hidden) {
+            return false;
+        } else {
+            // Temp set(will be used if only has one showing child)
+            onlyOneChild.value = item;
+            return true;
+        }
+    });
+
+    // When there is only one child router, the child router is displayed by default
+    if (showingChildren.length === 1) {
+        return true;
+    }
+
+    // Show parent if there are no child router to display
+    if (showingChildren.length === 0) {
+        onlyOneChild.value = { ...parent, path: '', noShowingChildren: true };
+        return true;
+    }
+
+    return false;
+};
+const resolvePath = routePath => {
+    if (isExternal(routePath)) {
+        return routePath;
+    }
+    if (isExternal(props.basePath)) {
+        return props.basePath;
+    }
+    return path.resolve(props.basePath, routePath);
+};
+</script>
+
 <template>
     <template v-if="!item.hidden">
         <li
@@ -35,72 +93,3 @@
         </el-sub-menu>
     </template>
 </template>
-
-<script>
-import path from 'path-browserify';
-import { isExternal } from '@/utils/validate';
-import Item from './Item.vue';
-import AppLink from './Link.vue';
-import FixiOSBug from './FixiOSBug';
-export default {
-    name: 'SidebarItem',
-    components: { Item, AppLink },
-    mixins: [FixiOSBug],
-    props: {
-        // route object
-        item: {
-            type: Object,
-            required: true,
-        },
-        isNest: {
-            type: Boolean,
-            default: false,
-        },
-        basePath: {
-            type: String,
-            default: '',
-        },
-    },
-    data() {
-        // To fix https://github.com/PanJiaChen/vue-admin-template/issues/237
-        // TODO: refactor with render function
-        this.onlyOneChild = null;
-        return {};
-    },
-    methods: {
-        hasOneShowingChild(children = [], parent) {
-            const showingChildren = children.filter(item => {
-                if (item.hidden) {
-                    return false;
-                } else {
-                    // Temp set(will be used if only has one showing child)
-                    this.onlyOneChild = item;
-                    return true;
-                }
-            });
-
-            // When there is only one child router, the child router is displayed by default
-            if (showingChildren.length === 1) {
-                return true;
-            }
-
-            // Show parent if there are no child router to display
-            if (showingChildren.length === 0) {
-                this.onlyOneChild = { ...parent, path: '', noShowingChildren: true };
-                return true;
-            }
-
-            return false;
-        },
-        resolvePath(routePath) {
-            if (isExternal(routePath)) {
-                return routePath;
-            }
-            if (isExternal(this.basePath)) {
-                return this.basePath;
-            }
-            return path.resolve(this.basePath, routePath);
-        },
-    },
-};
-</script>
