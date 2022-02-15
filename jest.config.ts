068ff520a3089a0fe4467fa4052f1abaf89379a1
“张智皓” <zhang948498@163.com>
2022-02-15 15:03:33 +0800

    test: 💍 添加组件单元测试

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-module.exports = {
+export default {
     moduleFileExtensions: ['js', 'ts', 'tsx', 'vue'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.vue$': '@vue/vue3-jest',
         '^.+\\.tsx?$': 'ts-jest',
     },
     testMatch: ['<rootDir>/tests/**/*.(spec|test).(ts|tsx)|**/__tests__/*.(ts|tsx)'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
